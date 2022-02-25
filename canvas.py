2750caa3b836818c5f2891b1c233e01aecae7833
Ali Shafiee <shafiee.ali10@gmail.com>
2022-02-25 19:55:41 +0330

    Change names of variables and functions based on clean code concepts.

--- canvas.py
+++ canvas.py
@@ -2,36 +2,37 @@ import enum
 from PyQt5 import QtWidgets, QtGui
 
 
-class toolMode(enum.Enum):
-   pen = 1
-   eraser = 2
-   fill = 3
+class ToolMode(enum.Enum):
+    pen = 1
+    eraser = 2
+    fill = 3
+
 
 class Canvas(QtWidgets.QLabel):
-    def __init__(self,W,H,backclr='#ffffff',pencolor='#000000'):
+    def __init__(self, w, h, back_clr='#ffffff', pen_color='#000000'):
         super().__init__()
-        self.canvas_Width = W
-        self.canvas_height = H
-        pixmap = QtGui.QPixmap(self.canvas_Width, self.canvas_height)
-        pixmap.fill(QtGui.QColor(backclr)) #background of paint
+        self.canvas_width = w
+        self.canvas_height = h
+        pixmap = QtGui.QPixmap(self.canvas_width, self.canvas_height)
+        pixmap.fill(QtGui.QColor(back_clr))  # background of paint
         self.setPixmap(pixmap)
         self.last_x, self.last_y = None, None
-        self.pen_color = QtGui.QColor(pencolor)
-        self.pen_Width = 1
-        self.mode = toolMode.pen
+        self.pen_color = QtGui.QColor(pen_color)
+        self.pen_width = 2
+        self.mode = ToolMode.pen
+
     def set_pen_color(self, color):
         self.pen_color = QtGui.QColor(color)
 
     def mouseMoveEvent(self, e):
-
-        if self.mode == toolMode.pen:
+        if self.mode == ToolMode.pen:
             if self.last_x is None:
                 self.last_x = e.x()
                 self.last_y = e.y()
                 return
             painter = QtGui.QPainter(self.pixmap())
             p = painter.pen()
-            p.setWidth(self.pen_Width)
+            p.setWidth(self.pen_width)
             p.setColor(self.pen_color)
             painter.setPen(p)
             painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
@@ -41,6 +42,6 @@ class Canvas(QtWidgets.QLabel):
             self.last_y = e.y()
 
     def mouseReleaseEvent(self, e):
-        if self.mode == toolMode.pen:
+        if self.mode == ToolMode.pen:
             self.last_x = None
             self.last_y = None
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from canvas import Canvas
 from PyQt5 import QtWidgets, uic  # pip install pyqt5
 import sys
-
 from qt_material import apply_stylesheet  # pip install qt_material
+
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('UI\\mainwindow.ui', self)
         apply_stylesheet(app, theme='dark_blue.xml')
-
-        self.addtionToUi()
+        self.addition_to_ui()
         self.show()
 
-    def addtionToUi(self):
+    def addition_to_ui(self):
         """this function used for ui objects or edits that cant done on QT creator"""
         self.penSizeCombobox.setStyleSheet("font-size: 20px;")
-        self.canvas = Canvas(1100,600)
+        self.canvas = Canvas(1100, 600)
         self.canvasHorizontalLayout.addWidget(self.canvas)
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
