efdfb08108b903d8938e7065034c3f3afa0738b4
ali shafiee <shafiee.ali10@gmail.com>
2022-03-12 07:00:25 -0500

    undo and redo action

--- canvas.py
+++ canvas.py
@@ -1,5 +1,7 @@
 import enum
 from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QShortcut, QAction
 
 
 class ToolMode(enum.Enum):
@@ -33,6 +35,11 @@ class Canvas(QtWidgets.QLabel):
         self.begin_shape_point = None
         self.end_shape_point = None
         self.before_drawing_shape_pixmap = None
+
+        self.undo_stack = list()
+        self.redo_stack = list()
+
+
 
     def set_pen_color(self, color):
         self.pen_color = QtGui.QColor(color)
@@ -94,6 +101,7 @@ class Canvas(QtWidgets.QLabel):
         pass
 
     def mousePressEvent(self, e):
+        self.undo_stack.append(self.pixmap().copy())
         if self.mode == ToolMode.pen:
             self.pen_mode_mouse_press_event(e)
         elif self.mode == ToolMode.eraser:
@@ -165,6 +173,19 @@ class Canvas(QtWidgets.QLabel):
         self.last_x = e.x()
         self.last_y = e.y()
 
+    def undo_action(self):
+        if self.undo_stack:
+            last_pixmap = self.undo_stack.pop()
+            self.redo_stack.append(self.pixmap().copy())
+            self.setPixmap(last_pixmap)
+            # self.update()
+
+    def redo_action(self):
+        if self.redo_stack:
+            last_pixmap = self.redo_stack.pop()
+            self.undo_stack.append(self.pixmap().copy())
+            self.setPixmap(last_pixmap)
+
     def eraser_mode_mouse_press_event(self, e):
         self.last_x = e.x()
         self.last_y = e.y()
@@ -180,13 +201,11 @@ class Canvas(QtWidgets.QLabel):
         self.last_x = e.x()
         self.last_y = e.y()
 
-
     def fill_mode_mouse_press_event(self, e):
         pass
-
-
 
     def mouseReleaseEvent(self, e):
         if self.mode == ToolMode.pen:
             self.last_x = None
             self.last_y = None
+        self.redo_stack = list()
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import PyQt5
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QShortcut, QAction
+
 from canvas import Canvas
-from PyQt5 import QtWidgets, uic  # pip install pyqt5
+from PyQt5 import QtWidgets, uic, QtGui, QtCore, Qt  # pip install pyqt5
 import sys
 from qt_material import apply_stylesheet  # pip install qt_material
 
@@ -12,6 +16,10 @@ class Ui(QtWidgets.QMainWindow):
         self.addition_to_ui()
         self.connect_btns_functions()
         self.show()
+        self.undo_shortcut = QShortcut(QKeySequence("Ctrl+Z"), self)
+        self.undo_shortcut.activated.connect(self.undo_shortcut_activate)
+        self.redo_shortcut = QShortcut(QKeySequence("Ctrl+Y"), self)
+        self.redo_shortcut.activated.connect(self.redo_shortcut_activate)
 
     def addition_to_ui(self):
         """this function used for ui objects or edits that cant done on QT creator"""
@@ -53,130 +61,116 @@ class Ui(QtWidgets.QMainWindow):
         #### end btn connections ####
 
 
+
     def new_file_btn_pressed(self):
-        print("newFileBtnPressed")
-
+        pass
     def open_file_btn_pressed(self):
-        print("openFileBtnPressed")
-
+        pass
     def save_file_btn_pressed (self):
-        print("saveFileBtnPressed")
+        pass
 
     def undo_btn_pressed(self):
-        print("undoBtnPressed")
+        self.canvas.undo_action()
 
     def redo_btn_pressed(self):
-        print("redoBtnPressed")
+        self.canvas.redo_action()
 
     def pen_btn_pressed(self):
         self.canvas.set_mode("pen")
-        print("penBtnPressed")
 
     def eraser_btn_pressed(self):
         self.canvas.set_mode("eraser")
-        print("eraserBtnPressed")
 
     def fill_btn_pressed(self):
         self.canvas.set_mode("fill")
-        print("fillBtnPressed")
 
     def pen_size_combobox_change(self):
         self.canvas.set_pen_size(int(self.penSizeCombobox.currentText()))
-        print(self.penSizeCombobox.currentText())
 
     def line_shape_btn_pressed(self):
         self.canvas.set_mode("shape")
         self.canvas.set_shape_mode("line")
-        print("lineShapeBtnPressed")
 
     def rounded_rect_shape_btn_pressed(self):
         self.canvas.set_mode("shape")
         self.canvas.set_shape_mode("rounded rect")
-        print("roundedSquareShapeBtnPressed")
 
     def rect_shape_btn_pressed(self):
         self.canvas.set_mode("shape")
         self.canvas.set_shape_mode("rect")
-        print("squareShapeBtnPressed")
 
     def circle_shape_btn_pressed(self):
         self.canvas.set_mode("shape")
         self.canvas.set_shape_mode("circle")
-        print("circleShapeBtnPressed")
 
     def change_selected_color_icon(self, color):
         self.selectedColorIcon.setStyleSheet("background-color:" + color + "; border-radius : 20;")
 
     def color0_btn_pressed(self):
         color = self.color0Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
 
     def color1_btn_pressed(self):
         color = self.color1Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
 
     def color2_btn_pressed(self):
         color = self.color2Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
 
 
     def color3_btn_pressed(self):
         color = self.color3Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
 
 
     def color4_btn_pressed(self):
         color = self.color4Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
 
 
     def color5_btn_pressed(self):
         color = self.color5Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
 
 
     def color6_btn_pressed(self):
         color = self.color6Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
 
 
     def color7_btn_pressed(self):
         color = self.color7Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
 
 
     def color8_btn_pressed(self):
         color = self.color8Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
-
 
     def color9_btn_pressed(self):
         color = self.color9Btn.palette().button().color().name()
-        print(str(color))
         self.change_selected_color_icon(color)
         self.canvas.set_pen_color(color)
 
-
     def color_picker_btn_pressed(self):
-        print("colorPickerBtnPressed")
+        pass
+
+    def undo_shortcut_activate(self):
+        self.undo_btn_pressed()
+
+
+    def redo_shortcut_activate(self):
+        self.redo_btn_pressed()
 
 
 app = QtWidgets.QApplication(sys.argv)
